e277e3447bb02fe95ec135611e0f31389918f64a
Luka Palelashvili <lukapalelashvili@gmail.com>
2022-07-09 22:12:05 +0400

    Landing page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import './App.css'
+import { ReactComponent as CrownIcon } from './assets/img/svg/crown.svg'
+import { ReactComponent as ArrowIcon } from './assets/img/svg/arrow.svg'
 
 function App() {
-	return <div className="text-red-700 font-bold text-3xl">Hii</div>
+	return (
+		<div className="flex h-full w-full">
+			<div className="bg-landing bg-cover h-screen w-1/2">
+				<div className="flex pl-16 gap-1 items-center h-20 bg-purple text-lg text-red text-white font-bold">
+					<CrownIcon className="inline-block align-middle" />
+					<span className="text-2xl">Redberry Knight Cup</span>
+				</div>
+			</div>
+			<div className="h-auto flex items-center w-1/2 bg-orange">
+				<div>
+					<h1 className="text-8xl text-white font-bold font-extrabold uppercase pl-20 leading-normal relative">
+						Chess says <br />
+						<span className="text-2xl absolute font-normal text-neutral-900 top-[19%] right-[-15%]">
+							a lot about
+						</span>
+						who we are
+					</h1>
+					<div className="pl-20 mt-20">
+						<button className="rounded-md flex items-center font-light gap-3 bg-black hover:bg-neutral-600 text-2xl font-['Open_Sans'] text-white py-3 px-6">
+							<span>Get Started</span> <ArrowIcon />
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	theme: {
+		extend: {
+			colors: {
+				purple: '#7025FB',
+				orange: '#FD5334',
+				black: '#212529',
+			},
+			backgroundImage: {
+				landing: "url('./assets/img/landing-page.png')",
+			},
+		},
+	},
+	plugins: [],
 }
