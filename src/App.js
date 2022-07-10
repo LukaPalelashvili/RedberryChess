aee50a40442c387e6f093ff4a7ef2e4e56088a2d
Luka Palelashvili <lukapalelashvili@gmail.com>
2022-07-10 23:54:16 +0400

    Change browserrouter to hash router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import './App.css'
 import { Home } from './pages/home/Home'
 import { PersonalInfo } from './pages/personal-info/PersonalInfo'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import { Experience } from './pages/chess-experience/Experience'
 import { Success } from './pages/success/Success'
 
 function App() {
 	return (
-		<BrowserRouter>
+		<HashRouter>
 			<Routes>
 				<Route index element={<Home />} />
 				<Route path="personal-info" element={<PersonalInfo />} />
 				<Route path="chess-experience" element={<Experience />} />
 				<Route path="success" element={<Success />} />
 			</Routes>
-		</BrowserRouter>
+		</HashRouter>
 	)
 }
 
